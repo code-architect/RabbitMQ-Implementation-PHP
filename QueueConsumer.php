d60e55e9d5712a51e2bbab3e73211e307687ca53
code-architect <codearchitectdeveloper@gmail.com>
2020-01-06 01:09:30 +0530

    Consuming published messages(basic)

---
+++ QueueConsumer.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+
+$conn = new \PhpAmqpLib\Connection\AMQPStreamConnection('localhost',5672,'guest', 'guest');
+
+$channel = $conn->channel();
+
+$channel->basic_consume('Queue-1', '', false, true, false, false, function ($msg){
+    echo 'Message Received :', $msg->body, '\n';
+});
+
+while (true) {
+    $channel->wait();
+}
+
+$channel->close();
+
